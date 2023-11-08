5331881abf555784a74535e4f943600ad67a4ae1
Klavs Tranborg <kte@scanitech.dk>
2023-11-08 16:06:39 +0100

    day 1

---
+++ infrastructure/Models/EventModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infrastructure.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public float Lat { get; set; }
+        public float Long { get; set; }
+        public DateTime CreatedAtUTC { get; set; }
+        public DateTime StartUTC { get; set; }
+        public DateTime EndUTC { get; set; }
+        public string Title { get; set; } 
+        public string Description { get; set; }
+        public int Owner { get; set; }
+        public int MaximumTickets { get; set; }
+        public int Sold { get; set; } // Calculated field
+        public int Available { get; set; } // Calculated field
+    }
+}
---
+++ infrastructure/Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infrastructure.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EMail { get; set; }
+        public string PasswordHash { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public float Lat { get; set; }
+        public float Long { get; set; }
+        public DateTime CreatedAtUTC { get; set; }
+    }
+}
