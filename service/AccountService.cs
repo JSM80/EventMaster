95da418098274653658d6ac75c939695b7240c92
JSM80 <mail@moellers-hule.dk>
2023-11-29 10:50:45 +0100

    JSM 03

---
+++ service/AccountService.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Authentication;
+using infrastructure.Models;
+using infrastructure.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace service;
+
+public class AccountService
+{
+    private readonly ILogger<AccountService> _logger;
+    private readonly PasswordHashRepository _passwordHashRepository;
+    private readonly UserRepository _userRepository;
+
+    public AccountService(ILogger<AccountService> logger, UserRepository userRepository,
+        PasswordHashRepository passwordHashRepository)
+    {
+        _logger = logger;
+        _userRepository = userRepository;
+        _passwordHashRepository = passwordHashRepository;
+    }
+
+    public UserModel? Authenticate(string email, string password)
+    {
+        try
+        {
+            var passwordHash = _passwordHashRepository.GetByEmail(email);
+            var hashAlgorithm = PasswordHashAlgorithm.Create(passwordHash.Algorithm);
+            var isValid = hashAlgorithm.VerifyHashedPassword(password, passwordHash.Hash, passwordHash.Salt);
+            if (isValid) return _userRepository.GetById(passwordHash.UserId);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Authenticate error: {Message}", e);
+        }
+
+        throw new InvalidCredentialException("Invalid credential!");
+    }
+
+    public UserModel Register(string fullName, string email, string password, string? avatarUrl)
+    {
+        var hashAlgorithm = PasswordHashAlgorithm.Create();
+        var salt = hashAlgorithm.GenerateSalt();
+        var hash = hashAlgorithm.HashPassword(password, salt);
+        var user = _userRepository.Create(firstName:);
+        _passwordHashRepository.Create(user.Id, hash, salt, hashAlgorithm.GetName());
+        return user;
+    }
+}
